d927d76d919df880fa4dc80a5311a316d5f0afb8
Alan Gonzalez <alanglez360@outlook.es>
2023-06-03 13:33:40 -0700

    Click en figuras con herramienta Cursor ahora selecciona la capa (detallitos); Propiedad GROSOR de contorno a las figuras

--- Jedialaa/public/js/painter.js
+++ Jedialaa/public/js/painter.js
@@ -1,5 +1,7 @@
 var dibujando = false
 
+var grosorDefault = 2
+
 var figuraSeleccionada = 'cursor'
 var arreglo = []
 var mouseXInicial
@@ -23,6 +25,8 @@ function setup() {
     var canvas = createCanvas(canvasWidth, canvasHeight)
     canvas.parent("canvasparent")
     canvas.mousePressed(presionado);
+
+    strokeWeight(grosorDefault)
 }
 
 function draw() {
@@ -30,6 +34,7 @@ function draw() {
     fill(255)
 
     arreglo.forEach(figura => {
+        strokeWeight(figura.grosor)
         if (figura.tipo == "linea") {
             line(figura.x1, figura.y1, figura.x2, figura.y2)
         }
@@ -40,6 +45,8 @@ function draw() {
             ellipse(figura.x, figura.y, figura.ancho, figura.alto)
         }
     });
+
+    strokeWeight(grosorDefault)
 
     if(dibujando){
         if(clickEnCanvas){
@@ -65,8 +72,6 @@ function draw() {
 function clickEnCanvas(){
     if (mouseXInicial > 0 && mouseXInicial < canvasWidth
         && mouseYInicial > 0 && mouseYInicial < canvasHeight
-        // && mouseXFinal > 0 && mouseXFinal < canvasWidth
-        // && mouseYFinal > 0 && mouseYFinal < canvasHeight
         ) {
         return true
     }
@@ -86,7 +91,12 @@ function mouseReleased() {
     mouseXFinal = mouseX
     mouseYFinal = mouseY
     if (clickEnCanvas()) {
-        crearNuevaFigura()
+        if(figuraSeleccionada == 'cursor'){
+            clickEnFigura()
+        }
+        else{
+            crearNuevaFigura()
+        }
     }
 
     mouseXInicial = undefined
@@ -98,39 +108,125 @@ function mouseReleased() {
 function crearNuevaFigura() {
     if (figuraSeleccionada == 'linea') {
         cantLinea++
+
+        // if(mouseXInicial > mouseXFinal || mouseYInicial > mouseYFinal){
+        //     [mouseYInicial, mouseYFinal] = [mouseYFinal, mouseYInicial];
+        //     [mouseXInicial, mouseXFinal] = [mouseXFinal, mouseXInicial];
+        // }
+        
         arreglo.push({
-            "nombre": "Linea "+cantLinea,
+            "nombre": "Line "+cantLinea,
             "x1": mouseXInicial,
             "y1": mouseYInicial,
             "x2": mouseXFinal,
             "y2": mouseYFinal,
+            "grosor": grosorDefault,
             "tipo": "linea"
         })
     }
     else if (figuraSeleccionada == 'rect') {
         cantRect++
+
+        if(mouseXInicial > mouseXFinal)
+            [mouseXInicial, mouseXFinal] = [mouseXFinal, mouseXInicial]
+        if(mouseYInicial > mouseYFinal)
+            [mouseYInicial, mouseYFinal] = [mouseYFinal, mouseYInicial]
+        
+
         arreglo.push({
-            "nombre": "Rectangulo "+cantRect,
+            "nombre": "Rectangle "+cantRect,
             "x": mouseXInicial,
             "y": mouseYInicial,
             "ancho": mouseXFinal - mouseXInicial,
             "alto": mouseYFinal - mouseYInicial,
+            "grosor": grosorDefault,
             "tipo": "rect"
         })
     }
     else if (figuraSeleccionada == 'circ') {
         cantCirc++
         arreglo.push({
-            "nombre": "Elipse "+cantCirc,
+            "nombre": "Ellipse "+cantCirc,
             "x": mouseXInicial + ((mouseXFinal - mouseXInicial) / 2),
             "y": mouseYInicial + ((mouseYFinal - mouseYInicial) / 2),
             "ancho": (mouseXFinal - mouseXInicial),
             "alto": (mouseYFinal - mouseYInicial),
+            "grosor": grosorDefault,
             "tipo": "circ"
         })
     }
+
     repintarBotonesCapas()
 
-    if(figuraSeleccionada!="cursor")
-        resaltarCapaCreada()
+    resaltarCapa(arreglo.length-1)
+}
+
+
+
+
+
+function clickEnFigura(){
+    for (let index = (arreglo.length-1); index >= 0; index--) {
+        if(arreglo[index].tipo == "rect"){
+            quitarActivoEnCapas()
+            if(clickEnRect(index)){
+                // alert("Click en "+arreglo[index].nombre)
+                resaltarCapa(index)
+                return
+            }
+        }
+        else if(arreglo[index].tipo == "circ"){
+            quitarActivoEnCapas()
+            if(clickEnCirc(index)){
+                // alert("Click en "+arreglo[index].nombre)
+                resaltarCapa(index)
+                return
+            }
+        }
+        else if(arreglo[index].tipo == "linea"){
+            quitarActivoEnCapas()
+            if(clickEnLinea(index)){
+                // alert("Click en "+arreglo[index].nombre)
+                resaltarCapa(index)
+                return
+            }
+        }
+    }
+}
+
+function clickEnRect(index) {
+    if(
+        (mouseXFinal >= arreglo[index].x - arreglo[index].grosor/2) && 
+        (mouseXFinal <= (arreglo[index].x + arreglo[index].ancho + arreglo[index].grosor/2)) &&
+        (mouseY >= arreglo[index].y - arreglo[index].grosor/2) && 
+        (mouseY <= (arreglo[index].y + arreglo[index].alto + arreglo[index].grosor/2))
+        ){
+            return true
+    }
+    return false
+}
+function clickEnCirc(index) {
+    var distance = Math.pow(mouseXFinal - arreglo[index].x, 2) / Math.pow((arreglo[index].ancho/2 + arreglo[index].grosor/2), 2) + Math.pow(mouseYFinal - arreglo[index].y, 2) / Math.pow((arreglo[index].alto/2 + arreglo[index].grosor/2), 2)
+    if(distance < 1){
+        return true 
+    }
+    return false
+}
+function clickEnLinea(index){
+    var tolerance = 4;
+    var m = (arreglo[index].y1 - arreglo[index].y2) / (arreglo[index].x1 - arreglo[index].x2);
+    var b = arreglo[index].y1 - (m * arreglo[index].x1);
+    var y = m * mouseXFinal + b;
+    var delta = Math.abs(y - mouseYFinal);
+
+    if (
+        (delta < (tolerance+arreglo[index].grosor/2)) && 
+        (mouseXFinal >= (arreglo[index].x1 - (tolerance+arreglo[index].grosor/2))) && 
+        (mouseXFinal <= (arreglo[index].x2 + (tolerance+arreglo[index].grosor/2))) && 
+        (mouseYFinal >= (arreglo[index].y1 - (tolerance+arreglo[index].grosor/2))) && 
+        (mouseYFinal <= (arreglo[index].y2 + (tolerance+arreglo[index].grosor/2)))
+    ){
+        return true
+    } 
+    return false
 }
