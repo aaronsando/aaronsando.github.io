bddb8ab9616c01733670cb59446c836a79c25281
Alan Gonzalez <alanglez360@outlook.es>
2023-06-05 23:39:49 -0700

    Input con metodo onchange para actualizar propiedades

--- Jedialaa/resources/views/comp/lineProperties.blade.php
+++ Jedialaa/resources/views/comp/lineProperties.blade.php
@@ -1,28 +1,28 @@
 <div id="line-properties" class="properties-panel" style="display:none">
-    <h1>LINEA</h1>
+    {{-- <h1>LINEA</h1> --}}
     <div class="input-group">
-        <span class="input-group-text">X</span>
-        <input id="line-x1" type="text" aria-label="X" class="form-control">
-        <span class="input-group-text">Y</span>
-        <input id="line-y1" type="text" aria-label="Y" class="form-control">
+        <span class="input-group-text">X1</span>
+        <input id="line-x1" onchange="actualizarPropiedades()" type="text" aria-label="X" class="form-control">
+        <span class="input-group-text">Y1</span>
+        <input id="line-y1" onchange="actualizarPropiedades()" type="text" aria-label="Y" class="form-control">
     </div>
     <br>
     <div class="input-group">
-        <span class="input-group-text">X</span>
-        <input id="line-x2" type="text" aria-label="X" class="form-control">
-        <span class="input-group-text">Y</span>
-        <input id="line-y2" type="text" aria-label="Y" class="form-control">
+        <span class="input-group-text">X2</span>
+        <input id="line-x2" onchange="actualizarPropiedades()" type="text" aria-label="X" class="form-control">
+        <span class="input-group-text">Y2</span>
+        <input id="line-y2" onchange="actualizarPropiedades()" type="text" aria-label="Y" class="form-control">
     </div>
     <br>
     <div class="input-group">
-        <span class="input-group-text">Border</span>
-        <input id="line-color-contorno" type="color" class="form-control form-control-color">
-        <input id="line-grosor-contorno" type="text" class="form-control">
+        <span class="input-group-text">Stroke</span>
+        <input id="line-grosor-contorno" onchange="actualizarPropiedades()" type="text" class="form-control">
+        <input id="line-color-contorno" onchange="actualizarPropiedades()" type="color" class="form-control form-control-color">
     </div>
     <br>
     <div class="input-group">
-        <label class="form-label text-light">Border Opacity</label>
-        <input id="line-opac-cont" type="range" min="0" max="255" class="form-range">
+        <label class="form-label text-light">Stroke Opacity</label>
+        <input id="line-opac-cont" onchange="actualizarPropiedades()" type="range" min="0" max="255" class="form-range">
     </div>
     <br>
 </div>
